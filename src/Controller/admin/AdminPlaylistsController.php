02a9f17b9b05de0d50edc0818834550fda13912e
Cosmiaou <160436481+Cosmiaou@users.noreply.github.com>
2025-11-21 18:03:56 +0100

    Phase 2 tache 1 : gestion des formations dans le back-office

---
+++ src/Controller/admin/AdminPlaylistsController.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Controller\admin;
+
+/**
+ * Description of AdminPlaylistsController
+ *
+ * @author louis
+ */
+class AdminPlaylistsController {
+    //put your code here
+}
---
+++ src/Form/FormationType.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categorie;
+use App\Entity\Formation;
+use App\Entity\Playlist;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use DateTime;
+
+class FormationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('publishedAt', DateType::class, [
+                'widget' => 'single_text',
+                'data' => isset($options['data']) &&
+                    $options['data']->getPublishedAt() != null ? $options['data']->getPublishedAt() : new DateTime('now'),
+                'label' => 'Date* :'
+            ])
+            ->add('title', TextType::class, [
+                'label' => 'Titre* :',
+                'attr' => [
+                    'maxlength' => 100,
+                    'minlenght' => 2
+                ]
+            ])
+            ->add('description', TextType::class, [
+                'label' => 'Description : ',
+                'required' => false
+            ])
+            ->add('videoId', TextType::class, [
+                'label' => 'URL de la vidéo* = www.youtube.com/',
+                'attr' => [
+                    'maxlength' => 12
+                ]
+            ])
+            ->add('playlist', EntityType::class, [
+                'class' => Playlist::class,
+                'label' => "Playlist(s)* :",
+                'choice_label' => 'name',
+            ])
+            ->add('categories', EntityType::class, [
+                'label' => "Catégorie(s)* :",
+                'class' => Categorie::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'required' => false
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Formation::class,
+        ]);
+    }
+}
