103e4eb32416bf595b6873791778bf8f8b53e65b
Cosmiaou <160436481+Cosmiaou@users.noreply.github.com>
2025-11-28 21:11:34 +0100

    Phase 4 partie 3 : second test déploiement continu

---
+++ bonjour.php
@@ -0,0 +1,5 @@
+<?php
+   echo " Bonjour. Site : <b>mediatekformation.autogend.fr</b> - Nous sommes le ".date ("Y-m-d H:i:s")."<br>";
+   echo "<br>";
+   echo "Version de PHP : <b>".phpversion()."</b> <br>";
+?>
